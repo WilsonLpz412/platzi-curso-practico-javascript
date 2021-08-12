9a6761b2d01dee41ad66cd178d89df8b63149568
WilsonLpz412 <wold412@gmail.com>
2021-08-11 19:15:39 -0500

    Taller #2: precios y descuentos: Array y switch de cupones

--- descuentos.js
+++ descuentos.js
@@ -12,11 +12,32 @@ function priceDiscount() {
   const inputPrice = document.getElementById("InputPrice");
   const priceValue = inputPrice.value;
 
-  const inputDiscount = document.getElementById("InputDiscount");
-  const discountValue = inputDiscount.value;
+  const InputCoupon = document.getElementById("InputCoupon");
+  const couponValue = InputCoupon.value;
 
-  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  let descuento;
+
+  switch(couponValue) {
+    case "JuanDC_es_Batman":
+      descuento = 15;
+    break;
+    case "pero_no_le_digas_a_nadie":
+      descuento = 30;
+    break;
+    case "es_un_secreto":
+      descuento = 25;
+    break;
+  }
+
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
   const resultPrice = document.getElementById("ResultPrice");
   resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
+
+const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
